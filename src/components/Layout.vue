5b98dacea19dc5b66265f61b170673a31eaa14f3
Rexie N. Villanueva <rexievillanueva03@gmail.com>
2025-07-22 22:37:39 +0800

    feat: add Profile and Sidenav components with icons and layout
    - Created Profile.vue component to display user profile information including picture and resume button.
    - Developed Sidenav.vue component featuring navigation icons and labels for Home, About, Achievements, Projects, and Blogs.
    - Introduced icon management in index.js to handle selected icon state.

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,54 @@
 <script setup>
+    import Profile from './ui/Profile.vue'
+    import Sidenav from './ui/Sidenav.vue';
 </script>
 
 <template>
+    <!--Main Layout-->
+    <section class="p-5 h-screen w-screen">
+        <div class="grid grid-cols-12 gap-5">
+            <!--Left Sidebar Section-->
+            <div class="h-screen bg-gray-800 rounded-2xl col-span-3">
+                <Profile />
+                <Sidenav />
+            </div>
+            <!--End Left Sidebar-->
 
+            <!--Middle Section-->
+            <div class="h-screen rounded-2xl col-span-6 space-y-2">
+                <!--Welcome Card-->
+                <div class="h-5/12 rounded-2xl bg-gray-800">
+                
+                </div>
+
+                <!--Career Stat-->
+                <div class="h-2/12 rounded-2xl bg-gray-800">
+                </div>
+
+                <!--Feautured-->
+                <div class="h-5/12 rounded-2xl bg-gray-800">
+                </div>
+            </div>
+            <!--End Middle -->
+
+            <!--Right Sidebar-->
+            <div class="h-screen rounded-2xl col-span-3 space-y-2">
+                <!--Skills-->
+                <div class="h-3/12 rounded-2xl bg-gray-800">
+                
+                </div>
+
+                <!--Expertise-->
+                <div class="h-5/12 rounded-2xl bg-gray-800">
+                
+                </div>
+
+                <!--Contacts-->
+                <div class="h-3/12 rounded-2xl bg-gray-800">
+                
+                </div>
+            </div>
+            <!--End of Right Sidebar-->
+        </div>
+    </section>
 </template>
---
+++ src/components/ui/Sidenav.vue
@@ -0,0 +1,54 @@
+<script setup>
+import Home from '../../assets/icons/home.svg'
+import HomeBlue from '../../assets/icons/home-blue.svg'
+import About from '../../assets/icons/profile.svg'
+import AboutBlue from '../../assets/icons/profile-blue.svg'
+import Medal from '../../assets/icons/medal.svg'
+import MedalBlue from '../../assets/icons/medal-blue.svg'
+import Project from '../../assets/icons/folder.svg'
+import ProjectBlue from '../../assets/icons/folder-blue.svg'
+import Blog from '../../assets/icons/blog.svg'
+import BlogBlue from '../../assets/icons/blog-blue.svg'
+
+import { selectedIcon } from '../../icon'
+
+const text = ['Home', 'About', 'Achivements', 'Projects', 'Blogs']
+const icons = [
+  Home, About, Medal, Project, Blog
+]
+const iconsBLue = [
+    HomeBlue, AboutBlue, MedalBlue, ProjectBlue, BlogBlue
+]
+
+
+</script>
+
+<template>
+  <section class="flex flex-col">
+    <div class="justify-start px-10 text-white">
+      <ul class="space-y-3">
+        <li class="flex space-x-2">
+            <img :src="icons[0]" alt="icon" class="w-7" />
+            <p>{{ text[0] }}</p>
+        </li>
+        <li class="flex space-x-2">
+            <img :src="icons[1]" alt="icon" class="w-7" />
+            <p>{{ text[1] }}</p>
+        </li>
+        <li class="flex space-x-2">
+            <img :src="icons[2]" alt="icon" class="w-7" />
+            <p>{{ text[2] }}</p>
+        </li>
+        <li class="flex space-x-2">
+            <img :src="icons[3]" alt="icon" class="w-7" />
+            <p>{{ text[3] }}</p>
+        </li>
+        <li class="flex space-x-2">
+            <img :src="icons[4]" alt="icon" class="w-7" />
+            <p>{{ text[4] }}</p>
+        </li>
+      </ul>
+    </div>
+    <div>Footer</div>
+  </section>
+</template>
