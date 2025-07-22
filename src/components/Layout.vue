afa5b99a77c3e26bc7c36439d28039abf5455a07
Rexie N. Villanueva <rexievillanueva03@gmail.com>
2025-07-22 22:50:34 +0800

    fix: adjust layout column spans for responsive design

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -8,14 +8,14 @@
     <section class="p-5 h-screen w-screen">
         <div class="grid grid-cols-12 gap-5">
             <!--Left Sidebar Section-->
-            <div class="h-screen bg-gray-800 rounded-2xl col-span-3">
+            <div class="h-screen bg-gray-800 rounded-2xl col-span-12 md:col-span-4">
                 <Profile />
                 <Sidenav />
             </div>
             <!--End Left Sidebar-->
 
             <!--Middle Section-->
-            <div class="h-screen rounded-2xl col-span-6 space-y-2">
+            <div class="h-screen rounded-2xl col-span-12 md:col-span-4 space-y-2">
                 <!--Welcome Card-->
                 <div class="h-5/12 rounded-2xl bg-gray-800">
                 
@@ -32,7 +32,7 @@
             <!--End Middle -->
 
             <!--Right Sidebar-->
-            <div class="h-screen rounded-2xl col-span-3 space-y-2">
+            <div class="h-screen rounded-2xl col-span-12 md:col-span-4 space-y-2">
                 <!--Skills-->
                 <div class="h-3/12 rounded-2xl bg-gray-800">
                 
