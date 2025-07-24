1738b0f7e11cdb483da679f175fc5733fd898b1a
Rexie N. Villanueva <rexievillanueva03@gmail.com>
2025-07-24 23:17:29 +0800

    feat: add Skill component to About page layout

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -2,21 +2,29 @@
     import CardAbout from './aboutUI/CardAbout.vue';
     import Contact from './aboutUI/Contact.vue';
     import AboutInfo from './aboutUI/AboutInfo.vue';
+    import Skill from '../ui/Skill.vue';
 </script>
 
 <template>
     <section>
         <!--Middle Section-->
         <div class="rounded-2xl col-span-12 md:col-span-4">
+            
             <!--About Card-->
             <div class="">
                 <CardAbout />
             </div>
-
+            
             <!--About Info-->
             <div class="rounded-2xl bg-gray-900 mt-2">
                 <AboutInfo />
             </div>
+            
+            <!--Skill-->
+            <div class="rounded-2xl bg-gray-900 mt-2 md:hidden">
+                <Skill />
+            </div>
+
             <!--Contact Info-->
             <div class="rounded-t-2xl bg-gray-900 mt-2">
                 <Contact />
